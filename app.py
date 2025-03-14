c3fde24967053173b468e4e78e07e4b0318dae4a
Nom075 <graytonl@nycstudents.net>
2025-03-14 10:15:00 -0400

    ah

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import json
 
+class Flashcard:
+    def __init__ (self, question, answer):
+        self.question = question
+        self.answer = answer
+    def display(self):
+        return (f"{self.question}? The answer is {self.answer}")
 
-ans = input("Are you a teacher or Student? (Input T or S)")
+
+
+""" ans = input("Are you a teacher or Student? (Input T or S)")
 if ans.upper() == "T":
     Pword = input("Enter valid teacher password")
     if Pword == "AAA":
@@ -13,5 +21,9 @@ if ans.upper() == "T":
 elif ans.upper() == "S":
     Role = "S"
 else:
-    print("Could not detect answer. Program shutting down...")
+    print("Could not detect answer. Program shutting down...") """
 
+
+
+Flashcards = [Flashcard(input("Enter a valid question"), input("Enter an answer"))]
+print(Flashcards)
