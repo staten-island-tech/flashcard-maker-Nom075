94d570f051a39c92231148cb9bc2403cbcc5f60f
Nom075 <graytonl@nycstudents.net>
2025-03-24 10:17:19 -0400

    3/24/25

--- app.py
+++ app.py
@@ -1,54 +1,64 @@
 import json
+import random
 
 class Flashcard:
-    def __init__ (self, question, answer):
+    def __init__(self, question, answer):
         self.question = question
         self.answer = answer
+
     def display(self):
-        return (f"{self.question} The answer is {self.answer}")
+        return f"Question: {self.question} The answer is {self.answer}"
 
-
-
-ans = input("Are you a teacher or Student? (Input T or S)")
+# Ask for role
+ans = input("Are you a teacher or Student? (Input T or S): ")
 if ans.upper() == "T":
-        Role = "T"
+    Role = "T"
 elif ans.upper() == "S":
     Role = "S"
 else:
     print("Could not detect answer. Program shutting down...")
+    exit()
 
 
-flash = [
-    Flashcard("Is Aaron dumb?", "Nah"),
-    Flashcard("What is the sin of 30?", "1/2"),
-    Flashcard("Darwin is dumb?", "Yes, he is.")
+try:
+    with open("flash.json", "r") as file:
+        flash_data = json.load(file)
+except (FileNotFoundError, json.JSONDecodeError):
+    flash_data = []
 
-]
+while True:
+    if Role == "T":
 
-flash_data = [cards.__dict__ for cards in flash]
-
-def Card_Loader(flash_data):
-    try:
-        with open("flash.json", "r") as file:
-            flash_data = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        flash_data = []
-
-    if not flash_data:
-        flash_data = [cards.__dict__ for cards in flash]
+        question = input("Enter a question: ")
+        answer = input("Now enter the answer: ")
+        flash_data.append({"question": question, "answer": answer})
 
 
-    with open("flash.json", "w") as file:
-        json.dump(flash_data, file, indent=4)
-
-    for card in flash:
-        print(card.display())
-
-    return flash_data
-
-flash_data = Card_Loader([])
+        with open("flash.json", "w") as file:
+            json.dump(flash_data, file, indent=4)
 
 
+        for card in flash_data:
+            print(f"{card['question']} The answer is {card['answer']}")
+
+        command = input("Got more cards? Y/N: ").strip().upper()
+  
+
+        if command != "Y":
+            break
+    
+    
+    if Role == "S":
+        try:
+            with open("flash.json", "r") as file:
+                flash_data = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            flash_data = []
+        
+        
 
 
-Card_Loader(flash_data)
+
+        break
+
+print("Program shutting down...")
